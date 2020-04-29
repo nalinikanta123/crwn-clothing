6893da485081c1cdf90947909f31409c7b008931
nalinikanta123 <nalinikantam@gmail.com>
2020-04-29 00:15:10 -0400

    final project ud lec 25

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,41 +3,40 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyAvD6BWddqZU4QsiB66b_8PlmfvKZ8ILkk",
-    authDomain: "crwn-db-74306.firebaseapp.com",
-    databaseURL: "https://crwn-db-74306.firebaseio.com",
-    projectId: "crwn-db-74306",
-    storageBucket: "crwn-db-74306.appspot.com",
-    messagingSenderId: "21100630330",
-    appId: "1:21100630330:web:dc2e94940f67bcd3000003",
-    measurementId: "G-MTNJEPGGX4"
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
 };
 
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
+  if (!userAuth) return;
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    const snapShot = await userRef.get();
+  const snapShot = await userRef.get();
 
-    if (!snapShot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            });
-        } catch (error) {
-            console.log('error creating user', error.message);
-        }
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
     }
+  }
 
-    return userRef;
+  return userRef;
 };
 
 export const auth = firebase.auth();
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { persistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
 
@@ -10,12 +10,12 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}>,
-  <BrowserRouter>
-      <persistGate persistor={persistor}>
+  <Provider store={store}>
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
         <App />
-      </persistGate>
-    </BrowserRouter>,
+      </PersistGate>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
